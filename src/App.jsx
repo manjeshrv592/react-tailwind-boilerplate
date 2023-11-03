dc411992f214b51521504c65391a44eda38d8321
Manjesh R V <manjeshrv592@gmail.com>
2023-11-03 10:50:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className='min-h-screen bg-gradient-to-br from-blue-300 to-blue-700 p-5 pt-20 text-center'>
+      <h1 className='text-4xl font-bold text-white lg:text-6xl'>
+        React & Tailwind Boilerplate
+      </h1>
+    </div>
+  );
+};
+
+export default App;
