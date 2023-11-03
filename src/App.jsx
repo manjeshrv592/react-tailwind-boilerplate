8441bf3d886e00c729c29e72b41e94f0ae357d72
Manjesh R V <manjeshrv592@gmail.com>
2023-11-03 11:08:21 +0530

    REstyled starter screen

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,16 @@ import React from "react";
 
 const App = () => {
   return (
-    <div className='min-h-screen bg-gradient-to-br from-blue-300 to-blue-700 p-5 pt-20 text-center'>
-      <h1 className='text-4xl font-bold text-white lg:text-6xl'>
+    <div className='min-h-screen bg-gray-950 p-5 pt-20 text-center'>
+      <h1 className='mb-10 bg-gradient-to-r  from-amber-600 to-amber-300 bg-clip-text text-4xl font-bold text-transparent lg:text-6xl'>
         React & Tailwind Boilerplate
       </h1>
+      <p className='mx-auto max-w-4xl text-xl leading-8 text-gray-400'>
+        This is a starter template for building React applications using Vite as
+        the build tool, Tailwind CSS for styling, ESLint for linting, and
+        Prettier for code formatting. It provides a quick setup for your React
+        projects, allowing you to focus on building your application features.
+      </p>
     </div>
   );
 };
